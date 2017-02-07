9d5662180c5c49a27be240f84acb601cf3ce4f64
Evgeny Tarasenko <e.tarasenko@octavianonline.com>
2017-02-07 17:29:48 +0300

    add task description

--- src/Messenger.h
+++ src/Messenger.h
@@ -1,6 +1,18 @@
 #ifndef _MESSENGER_H_
 #define _MESSENGER_H_
 
+#include <stdint.h>
+#include <unordered_map>
+
+/*
+Мессенжер (на примере WhatsApp, Viber, Telegram)
+* У меня есть список контактов, с кем я могу общаться
+* Могу отправить сообщение любому человеку из списка контактов
+* Могу создать чат с несколькими людьми и отправить сообщения им
+* Могу получать сообщения от других пользоватлей
+* Если у моего собеседника день рождения, месенжер предлагает отправить ему картинку с поздравлением или стикер
+*/
+
 
 class Date
 {
@@ -20,8 +32,24 @@ private:
 class Contact
 {
 private:
+    std::uint64_t m_id;
     Date m_date;
 };
+
+class ChatRoom
+{
+
+};
+
+class User
+{
+private:
+    std::unordered_map<Contact> m_contacts;
+    Contact my;
+
+
+};
+
 
 
 
