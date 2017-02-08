6dccf342306abb380335cca36ddb3ced9ddd2832
Evgeny Tarasenko <e.tarasenko@octavianonline.com>
2017-02-08 16:30:22 +0300

    add ICanToRecieveMessegeFromAnyOne correct ICanToSendMessegeToAnyoneInMyContactList to me check

--- test/MessengerTest.cpp
+++ test/MessengerTest.cpp
@@ -31,7 +31,27 @@ TEST_F(MessengerTest, ICanToSendMessegeToAnyoneInMyContactList) {
 
     me->sendMessege(tim_email, "Hello Tim!!!");
 
-    std::string expected { "my@email.com wrote: Hello Tim!!!" };
-    std::string result { tim->readP2PChat(my_email) };
+    std::string expected =  + "You wrote: Hello Tim!!!";
+    std::string result = me->readP2PChat(tim_email);
+    EXPECT_EQ(expected, result);
+}
+
+//Могу получать сообщения от других пользоватлей
+TEST_F(MessengerTest, ICanToRecieveMessegeFromAnyOne) {
+
+    std::string alex_email { "alex@email.com"};
+    std::string tim_email { "tim@email.com"};
+    std::string my_email { "my@email.com" };
+
+    UserSharedPtr me = std::make_shared<User>(my_email, Date("1", "may", "1980"));
+    UserSharedPtr alex = std::make_shared<User>(alex_email, Date("10", "july", "1990"));
+    UserSharedPtr tim = std::make_shared<User>(tim_email, Date("27", "march", "1987"));
+
+    tim->addContact(me->getMyContact());
+
+    tim->sendMessege(my_email, "Was Up?");
+
+    std::string expected = tim_email + " wrote: Was Up?";
+    std::string result = me->readP2PChat(tim_email);
     EXPECT_EQ(expected, result);
 }
